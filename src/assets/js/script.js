5d1ba278e695d20ff576cd0cf3431ede0074c876
Juan Prado <jprado@hugeinc.com>
2015-03-14 13:34:53 -0400

    adding pizzas function

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,12 +1,49 @@
-var IM_PROTO = (function() {
+var IM_PROTO = IM_PROTO || {};
 
-  return {
-    init: function() {
-      console.log('hello I am the prototype');
-    }
-  };
+
+IM_PROTO.getPizza = (function() {
+
+	function howManyPizzas(slices, people, isNYC) {
+
+		var averagePizzaSlice = isNYC ? 31.75 : 15, //area measured in square inches
+				num = slices * people, // get the number of slices
+				requiredPizza = averagePizzaSlice * num, // area of required pizza
+				mediumPizza = getPizzaArea(10), // diameter measured in inches
+				largePizza = getPizzaArea(14), // diameter measured in inches
+				extraLargePizza = getPizzaArea(18), // diameter measured in inches
+				mediumPizzas = 0,
+				largePizzas = 0,
+				extraLargePizzas = 0;
+				weNeedPizza = true;
+
+		while(weNeedPizza) {
+			if (requiredPizza <= mediumPizza) {
+				mediumPizzas ++;
+				weNeedPizza = false;
+			} else if (requiredPizza <= largePizza) {
+				largePizzas ++;
+				weNeedPizza = false;
+			} else if (requiredPizza <= extraLargePizza) {
+				extraLargePizzas ++;
+				weNeedPizza = false;
+			} else {
+				extraLargePizzas ++;
+				requiredPizza = requiredPizza - extraLargePizza;
+			}
+		}
+	}
+
+	function getPizzaArea(diameter) {
+		var z = diameter / 2,
+				pi = Math.PI,
+				a = 1;
+
+		return pi * (z * z) * a; // get it?
+	}
+
+	return {
+		play: playSound
+	};
 
 })();
-
-IM_PROTO.init();
 
